f7ef9fd7a6499d41d211deb7cbbe9aa40ceebd98
danilod100 <danilod100@924bc8c4-10ea-11de-8090-533a57e34bb9>
2009-03-15 19:59:40 +0000

    Simple updates

--- trunk/lincefr.c
+++ trunk/lincefr.c
@@ -5,14 +5,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "strings/lincefr.h"
+
 
 int main( int argc, char *argv[])
 {
   if( argc <= 1 )
    {
-     fprintf(stderr,lincefrstr1);
-     printf(lincefrstr2);
+     fprintf(stderr,"[ERROR] No input file!\n");
+     printf("Use: %s [FILENAME] [COMMAND]\n",argv[0]);
      return 1;
    }
 

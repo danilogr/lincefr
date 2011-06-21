e58a6c088b953c3d2489ea00cfdf097ff96d6f80
danilod100 <danilod100@gmail.com>
2011-06-21 21:50:25 +0000

    Remodelagem do Repositorio para as novas necessidades

---
+++ ProjetoFinal/ProjetoFinal.cpp
@@ -0,0 +1,86 @@
+// ProjetoFinal.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// Declara ponteiros para matrizes A,B,C,D
+	// Declara escalares
+
+	// ---------------------------------------
+	// Teste 1 - 4 x 4
+	// Alocação dinâmica de A,B,C,D
+	// Chama F1 - função em C
+	// Exibe resultado
+
+	// Chama F2 - função em C + ASM
+	// Exibe resultado
+
+	// Desaloca as matrizes A,B,C,D
+
+
+	// ---------------------------------------
+	// Teste 2 - 16 x 16
+	// Alocação dinâmica de A,B,C,D
+	// Chama F1 - função em C
+	// Exibe resultado
+
+	// Chama F2 - função em C + ASM
+	// Exibe resultado
+
+	// Desaloca as matrizes A,B,C,D
+
+	
+	
+	// ---------------------------------------
+	// Teste 3 - 64 X 64
+	// Alocação dinâmica de A,B,C,D
+	// Chama F1 - função em C
+	// Exibe resultado
+
+	// Chama F2 - função em C + ASM
+	// Exibe resultado
+
+	// Desaloca as matrizes A,B,C,D
+
+	// ---------------------------------------
+	// Teste 4 - 101 X 101
+	// Alocação dinâmica de A,B,C,D
+	// Chama F1 - função em C
+	// Exibe resultado
+
+	// Chama F2 - função em C + ASM
+	// Exibe resultado
+
+	// Desaloca as matrizes A,B,C,D
+
+
+
+
+	return 0;
+
+}
+/*
+
+void F1(*A, *B, *C, *D)
+{
+	// código
+	return
+}
+
+
+void F2(*A, *B, *C, *D)
+{
+	// código
+
+	__asm{
+
+
+	}
+
+	return
+}
+
+*/

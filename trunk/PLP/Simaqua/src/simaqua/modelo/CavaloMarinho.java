f0082a3c0e963f3739b4a76705d41e69361443f0
nelsonguicg@gmail.com <nelsonguicg@gmail.com@924bc8c4-10ea-11de-8090-533a57e34bb9>
2011-06-23 17:53:29 +0000

    Cavalo marinho

--- trunk/PLP/Simaqua/src/simaqua/modelo/CavaloMarinho.java
+++ trunk/PLP/Simaqua/src/simaqua/modelo/CavaloMarinho.java
@@ -4,10 +4,61 @@
  */
 package simaqua.modelo;
 
+import java.awt.Color;
+import simaqua.utils.Utilitarios;
+
 /**
  *
- * @author Nelson
+ * @author Daniel
  */
-public class CavaloMarinho {
+public class CavaloMarinho extends SerMarinho{
+    protected int posicaoInicialX,posicaoInicialY,dir;
+    
+    public CavaloMarinho(){
+        super(500,"S" ,Color.YELLOW);
+        posicaoInicialX = getX();
+        posicaoInicialY= getY();
+        
+        dir = Utilitarios.numeroAleatorio(0,3); // 0 = esquerda , 1 = direita , 2 = cima, 3 = baixo
+        
+    } 
+    
+    
+    public void movimento(){
+        int xAtual = getX();
+        int yAtual = getY();
+        
+        if (dir == 0) {
+            setX(xAtual-1);
+            
+        }
+        else if (dir == 1) {
+            setX(xAtual+1);
+        }
+        else if (dir == 2){
+            setY(yAtual+1);
+        }
+        else {
+            setY(yAtual-1);
+        }
+                    
+        
+        
+        int distPercorridaX = xAtual - getPosicaoInicialX();
+        if (Utilitarios.numeroAleatorio(0,1000) < distPercorrida){
+            setDir(1 - dir);
+            setPosicaoInicial(xAtual);
+        }
+        else if(dir == 0){
+            setX(xAtual-2);
+        }
+        else{
+            setX(xAtual+2);
+        }
+        
+        int movY = Utilitarios.numeroAleatorio(-10, 10);
+        setY(getY() + movY);
+
+    }
     
 }
